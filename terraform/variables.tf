ca1d63ae0c12daca3e490554ed481b65a2224918
Dawid Imbrzykowski <dawid.imb@gmail.com>
2025-07-04 13:24:22 +0200

    added proxmox provider to terraform. added variables. added files to gitignore.

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "pm_api_url" {
+    type = string
+    sensitive = false
+}
+
+variable "pm_api_token_id" {
+    type = string
+    sensitive = true
+}
+
+variable "pm_api_token_secret" {
+    type = string
+    sensitive = true
+}
+
+variable "pm_tls_insecure" {
+    type = bool
+    sensitive = true
+}
